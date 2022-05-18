db1cd37d4821ee7500c8be33cdeefca114098a32
Hubert Lemczak <hubertlemczak@gmail.com>
2022-05-18 11:20:11 +0100

    c

--- src/data-types/objects/object-keys.js
+++ src/data-types/objects/object-keys.js
@@ -5,32 +5,32 @@ const book = {
   category: 'Cooking',
   isbn: {
     isbn10: '9780132350884',
-    asin: '0132350882'
+    asin: '0132350882',
   },
   publisher: 'Prentice Hall',
-  dimensions: '10x12x2'
-}
+  dimensions: '10x12x2',
+};
 
-const isbn13 = '978-0132350884'
+const isbn13 = '978-0132350884';
 
 // TODO: write code in this section to pass the tests. You will need to add new code
 // as well as modify some of the existing code
 
 // Set this to the book name
-const name = book.name
+const name = book.name;
 
 // Set this to the isbn 10 value
-const isbn10 = book.isbn.isbn10
+const isbn10 = book.isbn.isbn10;
 
-book.category = 'Programming'
-book.pages = 464
-book.isbn.isbn13 = isbn13
-delete book.dimensions
-delete book.isbn.asin
+book.category = 'Programming';
+book.pages = 464;
+book.isbn.isbn13 = isbn13;
+delete book.dimensions;
+delete book.isbn.asin;
 
 // Do not edit this exported object
 module.exports = {
   name: name,
   isbn10: isbn10,
-  book: book
-}
+  book: book,
+};
